efcb512bda3c28e00a7c6790554578805fe4789f
Fernando Pineda <fernandopineda3105@gmail.com>
2020-10-09 11:09:06 -0500

    just comment

--- choreography-pattern/src/test/java/com/fpineda/samples/choreographypattern/service/CommitOrderUseCaseTest.java
+++ choreography-pattern/src/test/java/com/fpineda/samples/choreographypattern/service/CommitOrderUseCaseTest.java
@@ -1,3 +1,10 @@
+/**
+ * @author fpineda
+ * @email fernandopineda3105@gmail.com
+ * @create date 2020-10-09 11:08:46
+ * @modify date 2020-10-09 11:08:46
+ * @desc [description]
+ */
 package com.fpineda.samples.choreographypattern.service;
 
 public class CommitOrderUseCaseTest {
